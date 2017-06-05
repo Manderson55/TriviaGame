68ac55e38b39c52168bf7511ed82a6d1f475466a
Manderson55 <mariela.z.anderson@gmail.com>
2017-06-05 10:48:26 -0600

    compare function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,63 +7,60 @@ How many crayons The average child wears down by their 10th birthday? 720
 What color was the 100 billionth crayon made in 1996? blue ribbon
 when did Crayola changed the name of flesh to peach? 1962 
 */
+
+$(document).ready(function(){
 var questionsAnswers = [
     {
         askQuestion: "Who came up with the name Crayola?",
         choice1:  "Binney",
         choice2:  "Binney's wife",
         choice3:  "Smith",
-        choice4:  "Smith's wife",
-        answer:   "Binney's wife"
+        choice4:  "Smith's wife"
     },
     {
         askQuestion: "How much was the first crayon sold for?",
         choice1:  "1 cent",
         choice2:  "1 dime",
         choice3:  "1 nickel",
-        choice4:  "3 cents",
-        answer:   "1 nickel"
+        choice4:  "3 cents"
     },
     {
         askQuestion: "Where was the first crayola company?",
         choice1:  "Easton, PA",
         choice2:  "Bethlehem, PA",
         choice3:  "Philadelphia, PA",
-        choice4:  "Hershey, PA",
-        answer:   "Easton, PA"
+        choice4:  "Hershey, PA"
     },
         {
         askQuestion: "When was the first crayon made?",
         choice1:  "1900",
         choice2:  "1903",
         choice3:  "1905",
-        choice4:  "1907",
-        answer:   "1903"
+        choice4:  "1907"
     },
         {
         askQuestion: "How many colors do crayons come in?",
         choice1:  "72",
         choice2:  "86",
         choice3:  "120",
-        choice4:  "96",
-        answer:   "120"
+        choice4:  "96"
     },
         {
         askQuestion: "Which count box of crayons is the best seller?",
         choice1:  "48",
         choice2:  "36",
         choice3:  "12",
-        choice4:  "24",
-        answer:   "24"
+        choice4:  "24"
     }, 
         {
         askQuestion: "What is the favorite crayon color in the US?",
         choice1:  "yellow",
         choice2:  "green",
         choice3:  "blue",
-        choice4:  "red",
-        answer:   "blue"
+        choice4:  "red"
     }]
+
+    var answers = ["Binney's wife", '1 nickel', 'Easton, PA', '1903', '120', '24', 'blue'];
     var mainContent = $("<section>").attr({ // creating the html form dynamically for the answers
         "class":"questionAnswers",
         "id":"questionAnswers"
@@ -73,14 +70,13 @@ var questionsAnswers = [
     var wrongAnswers = 0;
     var unansweredQuestions = 0;
 
-
-$(document).ready(function(){
-  
     $("#instructions").html("<h4>" + "You have 20 seconds to finish this quiz. If you are done before the time is up, click the 'Submit Answers' button and the timer will stop and your questions will be submitted for verification. Good Luck!" + "</h4>" );
     var startButton = $("<button>").attr({"class": "btn btn-warning", //button to start the game and the timer
                                            "id" : "startClock"
                                         }).html("Start Playing!");
     $("#startButton").append(startButton);
+
+    resetGame();
 
     var submitAnswersButton = $("<button>").attr({"class": "btn btn-warning", //button to stop the game and the timer
                                    "id" : "submitAnswers"
@@ -104,24 +100,13 @@ $(document).ready(function(){
         }
     }); //end startCLock
 
-
-    $("#submitAnswers").click(function(){
-        console.log("clicked submit answers button");
-        stopTimer();
-        checkAnswers();
-        printResults();
-     }); //end 
-
-
     function stopTimer(){
         console.log("inside stopTimer function");
         clearInterval(counter);
         checkAnswers();
-        printResults();
-  
+        printResults();  
     }
    
-
     function printQuiz() {
         for (var y = 0; y < questionsAnswers.length; y++) { //call the function to print the questions
             printQuestions(y);
@@ -129,10 +114,13 @@ $(document).ready(function(){
        
         $("#quizForm").append(mainContent); //appending the main content area to the html window after loopin through array
 
- 
         mainContent.append(submitAnswersButton);
-
+        $("#submitAnswers").click(function(){
+        console.log("clicked submit answers button");
+        stopTimer();
+         }); 
     } //end printQuiz
+
     function printQuestions(y) { 
         var choices = [];                                   //this array will contain all the data
         questionsAnswers.forEach(function(question, index){
@@ -140,9 +128,6 @@ $(document).ready(function(){
         });
         var arrChoices = [choices[y].choice1, choices[y].choice2, choices[y].choice3, choices[y].choice4];
         console.log(choices[y]);
-        var correctAnswers = []; 
-        correctAnswers.push(choices[y].answer);
-        console.log(correctAnswers);       
 
         var quizQuestion = $("<p>").html(choices[y].askQuestion);
         mainContent.append(quizQuestion); //appending the question to the main content area
@@ -169,34 +154,29 @@ $(document).ready(function(){
      } //end printAnswers
 
     function checkAnswers(){
-//I want to go through each question/answer section and if the user 
-// selected somenthing , check if the value is correct
         console.log("inside checkAnswers");
-        for (var z=0; z < questionsAnswers.length; z++) { 
-            console.log("Question # " + z );
-            console.log("looping through the questions"); 
 
-        if (!($("input[id = 'radio + z + 1']:checked")) 
-         && !($("input[id = 'radio + z + 2']:checked"))
-          && !($("input[id = 'radio + z + 3']:checked"))
-           && !($("input[id = 'radio + z + 4']:checked"))){
-
-                console.log("user didn't select an answer!")
-                unansweredQuestions++;
-                console.log("Unanswered Questions = " + unansweredQuestions);
-
-        } else if 
-                ($("input[id = 'radio + z + 1']:checked")) {               
-                        if ($("input[value]") === correctAnswers[z]) {
-                            correctAnswers++;
-                            console.log("correct answers  " + correctAnswers);
-                        }else {
-                             wrongAnswers++
-                             console.log("wrong answers   " + wrongAnswers);
-                        }              
-                }                     
-
-        }  
+        var userAnswers = []; // create an array with the user choices checking which
+        for(var i = 0; i < 7; i++){ //input radio button is checked
+            var userChoice = $("input:radio[name=optradio" + i +  "]:checked").val(); // dynamically added
+            console.log(userChoice);
+            userAnswers.push(userChoice);
+            }
+        console.log(userAnswers);
+        console.log(answers);
+            // Comparing
+        for(var i = 0; i < userAnswers.length; i++){
+            if (userAnswers[i] === answers[i] ){
+                console.log("Right Answer: " + userAnswers[i]);
+                correctAnswers++;
+            } else if(userAnswers[i] === undefined){
+                        console.log("user didn't select an answer!")
+                        unansweredQuestions++;  
+                    } else{
+                           wrongAnswers++;
+                           console.log("Wrong: " + userAnswers[i]);
+                          }
+        }
 
     }//end check asnwers
 
@@ -204,12 +184,17 @@ $(document).ready(function(){
  
        console.log("game over!");
        (mainContent).hide;
-
-    $("#quizForm").html("<h3>" + "Correct Answers: "+  correctAnswers + "</h3>" +
-     "<h3>" + "Wrong Answers: "+  wrongAnswers + "</h3>" +
-     "<h3>" + "Unanswered Questions: " +  unansweredQuestions + "</h3>");
-
  
+        $("#quizForm").html("<h3>" + "Correct Answers: "+  correctAnswers + "</h3>" +
+         "<h3>" + "Wrong Answers: "+  wrongAnswers + "</h3>" +
+         "<h3>" + "Unanswered Questions: " +  unansweredQuestions + "</h3>");
+       
+    } // end print results
+
+    function resetGame(){
+        correctAnswers = 0;
+        wrongAnswers = 0;
+        unansweredQuestions = 0;
     }
 
 
